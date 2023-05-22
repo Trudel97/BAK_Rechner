d9aba4a493ee45c9d63f7972b7f322c5cea72482
Trudel97 <valentin.m.schmidt97@gmail.com>
2023-05-22 10:56:08 +0200

    es Funnktioniert!

--- BKA.py
+++ BKA.py
@@ -15,24 +15,23 @@ def auswahl_Reduktionsfaktor():
 
     else:
         print ('Bitte geben Sie an ob sie weiblich oder männlich sind')
+        return auswahl_Reduktionsfaktor
         
     
 def berechnungALK(): #ml*(alk in % / 100)*0,8g/cm^3 / Berechnung des getrunkenen Alkohols
     alk_Menge = float(input('Bitte geben Sie Ihren Konsum in ml an'))*\
-                float(input('Bitte geben Sie den Alkoholanteil in Prozen an'))*0,8
+                float(input('Bitte geben Sie den Alkoholanteil in Prozen an'))*0.8
     return alk_Menge
 
-
-
 #Berechnung der Promille = getrunkener Alk / (gewicht * reduktionsfaktor)
-def berechnung_Promille(alk_Menge, gewicht, geschlecht):
+def berechnung_Promille(geschlecht, alk_Menge, gewicht):
     Promille = alk_Menge / (gewicht*geschlecht)
     return Promille
     
-def main(Promille):
+def main():
     geschlecht = auswahl_Reduktionsfaktor()
     alk_Menge = berechnungALK ()
     Promille = berechnung_Promille(geschlecht, alk_Menge, gewicht)
-    print(Promille)
+    print(f"Die Blutalkoholkonzentration beträgt {Promille: .2f} Promille")
 
-main(Promille)
+main()
